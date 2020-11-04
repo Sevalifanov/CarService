08a30bbd9300c105f237d3162be701aba46259da
Лифанов Всеволод Вячеславович <v.lifanov@i-teco.ru>
2020-11-04 20:51:12 +0300

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	.idea/workspace.xml
    #	target/classes/com/spring/carService/Main.class
    #	target/classes/com/spring/carService/Service/GarageService.class
    #	target/classes/com/spring/carService/Service/GarageServiceImpl.class

--- src/main/java/com/spring/carservice/controller/GreetingController.java
+++ src/main/java/com/spring/carservice/controller/GreetingController.java
@@ -8,8 +8,6 @@ public class GreetingController {
 
     @GetMapping
     public String greeting() {
-        return "welcome to garage \n" +
-                "POST /addCar  BODY: {\"id\":121,\"brand\":\"укажи имя\",\"modelName\":\"укажи модель\"}\n" +
-                "POST /addMechanic BODY: {\"id\": 1 ,\"firstName\": \"IVANOVICH\",\"lastName\": \"IVANOV\"}";
+        return "welcome to garage";
     }
 }
