e03356b00188055cc2dca4d4c489d235a3989458
Лифанов Всеволод Вячеславович <v.lifanov@i-teco.ru>
2020-12-02 22:13:09 +0300

    task five

---
+++ CarServiceNew/src/main/java/com/spring/carservice/configuration/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.spring.carservice.configuration;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource(Environment environment) {
+        DriverManagerDataSource dataSource = new SingleConnectionDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("datasource.driver"));
+        dataSource.setUrl(environment.getRequiredProperty("datasource.url"));
+        dataSource.setUsername(environment.getRequiredProperty("datasource.username"));
+        dataSource.setPassword(environment.getRequiredProperty("datasource.password"));
+        ((SingleConnectionDataSource) dataSource).setSuppressClose(true);
+        return dataSource;
+    }
+}
