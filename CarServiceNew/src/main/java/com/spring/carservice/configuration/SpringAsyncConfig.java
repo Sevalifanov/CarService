5ea013a92543247063719f7b8270e07d0604a4d7
Лифанов Всеволод Вячеславович <v.lifanov@i-teco.ru>
2021-01-06 17:05:38 +0300

    Merge branch 'main' of https://github.com/Sevalifanov/CarService into main
    # Conflicts:
    #	CarServiceNew/src/main/java/com/spring/carservice/configuration/ApplicationConfig.java
    #	CarServiceNew/src/main/java/com/spring/carservice/configuration/SpringAsyncConfig.java
    #	CarServiceNew/src/main/resources/application.properties
    #	CarServiceNew/src/main/resources/db/changelog/v1.0/orders.xml

--- CarServiceNew/src/main/java/com/spring/carservice/configuration/SpringAsyncConfig.java
+++ CarServiceNew/src/main/java/com/spring/carservice/configuration/SpringAsyncConfig.java
@@ -4,27 +4,26 @@ import com.spring.carservice.exeption.AsyncExceptionHandler;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@PropertySource("classpath:application.properties")
 @Configuration
-@EnableAsync
 public class SpringAsyncConfig implements AsyncConfigurer {
 
     @Value("${thread.corePoolSize}")
-    private int corePoolSize;
-
+    private String corePoolSize;
     @Value("${thread.maxPoolSize}")
-    private int maxPoolSize;
+    private String maxPoolSize;
 
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(corePoolSize);
-        threadPoolTaskExecutor.setMaxPoolSize(maxPoolSize);
+        threadPoolTaskExecutor.setCorePoolSize(Integer.valueOf(corePoolSize));
+        threadPoolTaskExecutor.setMaxPoolSize(Integer.valueOf(maxPoolSize));
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
     }
@@ -33,5 +32,6 @@ public class SpringAsyncConfig implements AsyncConfigurer {
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return new AsyncExceptionHandler();
     }
+
 
 }
